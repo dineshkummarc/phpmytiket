ad65b8156aa02d21232d2a58709cc6b3ea4f4ea6
Dinesh Kumar <mapmydestination@gmail.com>
2024-11-07 22:09:25 +0530

    updated

---
+++ application/logs/log-2024-11-07.php
@@ -0,0 +1,48 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-11-07 16:04:10 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:04:13 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'tiket_demo' C:\xampp\htdocs\phpmytiket\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-11-07 16:04:13 --> Unable to connect to the database
+ERROR - 2024-11-07 16:06:31 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:06:31 --> Severity: Warning --> Use of undefined constant DONATION_MODULE - assumed 'DONATION_MODULE' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\models\user\constants.php 16
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 11
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 13
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 15
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 19
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 21
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 23
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 3
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 4
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 5
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 6
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 7
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 8
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 10
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> include_once(./vendor/autoload.php): failed to open stream: No such file or directory C:\xampp\htdocs\phpmytiket\index.php 289
+ERROR - 2024-11-07 16:06:32 --> Severity: Warning --> include_once(): Failed opening './vendor/autoload.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\phpmytiket\index.php 289
+ERROR - 2024-11-07 16:08:51 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant DONATION_MODULE - assumed 'DONATION_MODULE' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\models\user\constants.php 16
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 11
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 13
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 15
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 19
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 21
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\header.php 23
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 3
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 4
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 5
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 6
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 7
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 8
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> Use of undefined constant BASE_URL - assumed 'BASE_URL' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\phpmytiket\application\views\auth\footer.php 10
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> include_once(./vendor/autoload.php): failed to open stream: No such file or directory C:\xampp\htdocs\phpmytiket\index.php 289
+ERROR - 2024-11-07 16:08:51 --> Severity: Warning --> include_once(): Failed opening './vendor/autoload.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\phpmytiket\index.php 289
+ERROR - 2024-11-07 16:14:10 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:14:51 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:14:51 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:15:13 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:15:14 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:15:14 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:15:14 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:15:22 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
+ERROR - 2024-11-07 16:15:33 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\phpmytiket\application\vendor/autoload.php was not found.
